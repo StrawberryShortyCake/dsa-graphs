ff320bc2e9e13ad14acc90eb83c3ae9d8c4f4bb6
Andrea Jao <summamazing@gmail.com>
2024-07-11 15:52:44 -0700

    completed fewestEdges

--- graphs/fewest-edges/fewestEdges.ts
+++ graphs/fewest-edges/fewestEdges.ts
@@ -1,9 +1,32 @@
 import { UGraphNodeStr } from "../common/graph";
+import { Queue } from "../common/queue";
 
 /** Number of fewest edges between start and end.
  * If no path can be found, return Infinity. */
 
 function fewestEdges(start: UGraphNodeStr, sought: UGraphNodeStr): number {
+
+  if (start === sought) return 0
+
+    const toVisit = new Queue([{node: start, depth:0}])
+    const visited: UGraphNodeStr[] = []
+
+    while (!toVisit.isEmpty()){
+      const visitingNode = toVisit.dequeue()
+
+      if (visitingNode.node.value === sought.value){
+        return visitingNode.depth
+      }
+
+      for (const neighbor of visitingNode.node.adjacent){
+        if (!visited.includes(neighbor)){
+          toVisit.enqueue({node: neighbor, depth: visitingNode.depth+1})
+          visited.push(neighbor)
+        }
+      }
+  }
+
+
   return Infinity;
 }
 
